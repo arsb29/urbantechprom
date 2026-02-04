e382931403bc024f644fc05a046b87851f5a3326
arseny <ars-bel@mail.ru>
2026-02-05 00:07:17 +0300

    HeroSection

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -2,27 +2,15 @@ import styles from './HeroSection.module.css';
 
 export function HeroSection() {
   return (
-    <section className={styles.hero}>
-      <header className={styles.heroHeader}>
-        <div className={styles.heroLogo}>
-          <span className={styles.heroLogoIcon}>◎</span>
-          <span className={styles.heroLogoText}>УРБАНТЕХПРОМ</span>
-        </div>
-        <button className={styles.heroBtnContact}>Связаться нами</button>
-      </header>
-
-      <div className={styles.heroContent}>
-        <div className={styles.heroText}>
-          <h1>Корпоративный центр<br/>компетенций <span className={styles.heroHighlight}>ГК &quot;Урбантех&quot;</span></h1>
-          <p>Инженерные, инвестиционные и управленческие<br/>
-          ресурсы для реализации проектов в сфере<br/>
-          создания вычислительных комплексов<br/>
-          и объектов газовой генерации</p>
-        </div>
-        <div className={styles.heroImage}>
-          <div className={styles.heroTurbineIcon}></div>
-        </div>
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <h1 className={styles.title}>Корпоративный центр компетенций <span className={styles.highlight}>ГК &quot;Урбантех&quot;</span></h1>
+        <p className={styles.description}>Инженерные, инвестиционные и управленческие
+        ресурсы для реализации проектов в сфере
+        создания вычислительных комплексов
+        и объектов газовой генерации
+        </p>
       </div>
-    </section>
+    </div>
   );
 }
