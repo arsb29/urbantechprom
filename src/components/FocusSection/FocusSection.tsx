a6ba9915c8b88909d72cd4dc8dd8505f01221b83
arseny <ars-bel@mail.ru>
2026-02-05 00:30:47 +0300

    focus

--- src/components/FocusSection/FocusSection.tsx
+++ src/components/FocusSection/FocusSection.tsx
@@ -1,32 +1,26 @@
 import styles from './FocusSection.module.css';
+import Logo from '@/assets/corner.svg';
 
 export function FocusSection() {
   return (
-    <section className={styles.focus}>
-      <header className={styles.focusHeader}>
-        <div className={styles.focusLogo}>
-          <span className={styles.focusLogoIcon}>◎</span>
-          <span className={styles.focusLogoText}>УРБАНТЕХПРОМ</span>
+    <div className={styles.container}>
+      <h2 className={styles.title}>Фокус компании</h2>
+      <div className={styles.list}>
+        <div className={styles.row}>
+          <Logo />
+          <div className={styles.description}>Повышение инвестиционной эффективности инфраструктурных вычислительных центров</div>
         </div>
-        <button className={styles.focusBtnContact}>Связаться нами</button>
-      </header>
-
-      <h2 className={styles.focusTitle}>Фокус компании</h2>
-
-      <div className={styles.focusList}>
-        <div className={styles.focusItem}>
-          <div className={styles.focusCheckbox}></div>
-          <p>Повышение инвестиционной эффективности инфраструктурных<br/>вычислительных центров</p>
+        <div className={styles.hr} />
+        <div className={styles.row}>
+          <Logo />
+          <div className={styles.description}>Формирование технологической и финансовой базы для развития цифровой экономики</div>
         </div>
-        <div className={styles.focusItem}>
-          <div className={styles.focusCheckbox}></div>
-          <p>Формирование технологической и финансовой базы для развития<br/>цифровой экономики</p>
-        </div>
-        <div className={styles.focusItem}>
-          <div className={styles.focusCheckbox}></div>
-          <p>Сокращение сроков вывода мощностей на рынок</p>
+        <div className={styles.hr} />
+        <div className={styles.row}>
+          <Logo />
+          <div className={styles.description}>Сокращение сроков вывода мощностей на рынок</div>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
