b989b4d004227b00ebc68d07b2e6f6aa5a7c9a98
arseny <ars-bel@mail.ru>
2026-02-06 19:14:29 +0300

    фикс

--- src/components/FocusSection/FocusSection.tsx
+++ src/components/FocusSection/FocusSection.tsx
@@ -8,17 +8,17 @@ export function FocusSection() {
       <h2 className={styles.title}>Фокус компании</h2>
       <div className={styles.list}>
         <div className={styles.row}>
-          <Icon component={Logo} size={18} />
+          <Icon component={Logo} size={18} color="black" />
           <div className={styles.description}>Повышение инвестиционной эффективности инфраструктурных вычислительных центров</div>
         </div>
         <div className={styles.hr} />
         <div className={styles.row}>
-          <Icon component={Logo} size={18} />
+          <Icon component={Logo} size={18} color="black" />
           <div className={styles.description}>Формирование технологической и финансовой базы для развития цифровой экономики</div>
         </div>
         <div className={styles.hr} />
         <div className={styles.row}>
-          <Icon component={Logo} size={18} />
+          <Icon component={Logo} size={18} color="black" />
           <div className={styles.description}>Сокращение сроков вывода мощностей на рынок</div>
         </div>
       </div>
